c7fa1290010d171fec3d945264bbddb46747b33a
Maria Sanli <marisanli.93@gmail.com>
2023-10-20 22:31:18 -0400

    made cat filter button

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ const pets = [
     specialSkill:
       "Just picks the tomatoes off of a sandwich instead of requesting a whole new sandwich.",
     type: "dino",
-    imageUrl: "https://images.vexels.com/media/users/3/293984/isolated/lists/36e69f2873ebfb7de7abb5473fa5333a-cute-self-esteem-dino-character.png",
+    imageUrl:
+      "https://images.vexels.com/media/users/3/293984/isolated/lists/36e69f2873ebfb7de7abb5473fa5333a-cute-self-esteem-dino-character.png",
   },
   {
     id: 3,
@@ -142,7 +143,7 @@ const pets = [
     specialSkill: "Drives at a safe rate of speed in snow or rain.",
     type: "dino",
     imageUrl:
-      "https://images.readwrite.com/wp-content/uploads/2018/03/t-rex-dino-quiz-e1490854556549.jpg",
+      "https://cdn.paleo.gg/games/jwpo/images/dino/tyrannosaurus_rex.png",
   },
   {
     id: 17,
@@ -151,7 +152,7 @@ const pets = [
     specialSkill:
       "Does not freak out if you haven’t seen his favorite movie (The Big Lebowski).",
     type: "cat",
-    imageUrl: "http://www.funnycatsite.com/pictures/Close_Up_Yawn.jpg",
+    imageUrl: "https://i.pinimg.com/474x/51/41/c3/5141c3ae269e4382846ac3067cbbf9a9.jpg",
   },
   {
     id: 18,
@@ -160,7 +161,7 @@ const pets = [
     specialSkill: "Proficient in air guitar",
     type: "dino",
     imageUrl:
-      "https://www.nation.co.ke/image/view/-/4078922/highRes/1742693/-/maxw/600/-/1453yvh/-/DINO.jpg",
+      "https://cdn.paleo.gg/games/jwpo/images/dino/tyrannosaurus_rex.png",
   },
   {
     id: 19,
@@ -169,7 +170,7 @@ const pets = [
     specialSkill: "Listens attentively to boring stories.",
     type: "dog",
     imageUrl:
-      "http://dailynewsdig.com/wp-content/uploads/2014/03/Creative-And-Funny-Dog-Stock-Photography-Pictures-2.jpg",
+      "https://res.cloudinary.com/everydotorg/image/upload/f_auto,c_limit,w_256,q_auto/faja_profile/oovkuautvjurvb0gkku5",
   },
   {
     id: 20,
@@ -177,7 +178,8 @@ const pets = [
     color: "Black",
     specialSkill: "Uses litter box at appropriate hours.",
     type: "cat",
-    imageUrl: "http://www.funnycatsite.com/pictures/Lazy_White_Cat.jpg",
+    imageUrl:
+      "https://i.pinimg.com/474x/74/94/da/7494da32cf94655355b9334f95fd7dfe.jpg",
   },
   {
     id: 21,
@@ -186,7 +188,7 @@ const pets = [
     specialSkill: "Owns a Nintendo Power Glove.",
     type: "dino",
     imageUrl:
-      "https://img.buzzfeed.com/buzzfeed-static/static/2015-11/2/12/enhanced/webdr15/anigif_enhanced-29802-1446485228-10.gif?crop=250:165;0,0&downsize=715",
+      "https://cdn.pixabay.com/photo/2012/04/30/11/08/brown-44833_960_720.png",
   },
   {
     id: 22,
@@ -195,7 +197,7 @@ const pets = [
     specialSkill: "Is comfortable with jokes about his receding hairline.",
     type: "cat",
     imageUrl:
-      "http://funnyanimalphoto.com/wp-content/uploads/2013/08/cat_caught_mouse_thegatewaypundit.jpg",
+      "https://styles.redditmedia.com/t5_3fvdo5/styles/communityIcon_d99w7zbgzdo71.png?width=256&s=a39f78f98da5370c2b427adbf70db3cdbd701d46",
   },
   {
     id: 23,
@@ -214,7 +216,7 @@ const pets = [
       "Participates in karaoke but does not force others to go out to karaoke.",
     type: "dog",
     imageUrl:
-      "http://www.dogbreedplus.com/dog_breeds/images/basset-hound-4.jpg",
+      "https://i.pinimg.com/474x/88/25/21/882521a6c1fc9b389949700558a1ab25.jpg",
   },
   {
     id: 25,
@@ -223,7 +225,7 @@ const pets = [
     specialSkill: "Knows the words to 4 rap songs.",
     type: "cat",
     imageUrl:
-      "http://funbk.s3.amazonaws.com/wp-content/uploads/2016/06/funny-cat-video-which-will-make-you-laugh-louder.jpg",
+      "https://is5-ssl.mzstatic.com/image/thumb/Purple118/v4/2b/46/49/2b464997-a414-6956-7975-0968fda28fce/source/256x256bb.jpg",
   },
   {
     id: 26,
@@ -268,37 +270,65 @@ const pets = [
     specialSkill: "Doesn't get weirded out by the word 'moist.'",
     type: "dino",
     imageUrl:
-      "http://lsae2.iypcdn.com/static//modules/uploads/photos/language1/dino-live-22.jpg?119",
+      "https://ih1.redbubble.net/image.1974503408.9538/st,small,507x507-pad,600x600,f8f8f8.jpg",
   },
 ];
 
-const app = document.querySelector("#app");
 
-let domString = "";
-
-for (pet of pets) {
-  domString += `
-    <div class="card" style="width: 18rem;">
+// FUNCTION TO RENDER CARDS TO DOM THAT TAKES AN ARRAY
+const renderToDom = (pets) => {
+  //THIS IS DOMSTRING SO WE CAN PUSH CARDS TO IT
+  let domString = "";
+  //FOR EACH LOOP TO LOOP OVER ARRAY AND CREAT PET CARDS
+  pets.forEach((pet) => {
+    domString += `<div class="card" style="width: 18rem;">
+  <div class="card-header">${pet.name}</div>
   <img src="${pet.imageUrl}" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">${pet.name}</h5>
+    <h5 class="card-title">${pet.color}</h5>
     <p class="card-text">${pet.specialSkill}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="card-footer">${pet.type}</div>
   </div>
-</div>
-    `;
-}
+</div>`;
+  });
 
-app.innerHTML = domString;
+  //SELECT HTML app DIV
+  const app = document.querySelector("#app");
+  //NOW SET CARDS TO OUR APP DIVS INNER HTML**********
+  app.innerHTML = domString;
+};
+//RENDER AKA PUT CARDS ON THE PAGE 
+renderToDom(pets);
 
-// for (let i = 0; i < pets.length; i++;){
-//   console.log([i]);
-//   console.log(pets.[i].key);
-// }
-// ****for (pet of pets){
-//   console.log(pet.color);
-// }
 
-// pets.forEach((pet) => {
-//   console.log(pet.specialSkill)
-// })
+//NEED FILTER BUTTON FOR CATS ONLY
+//FIRST SELECT THAT HTML BUTTON? 
+const catButton = document.querySelector("#cat-button");
+
+//CREATE FUNCTION TO FILTER OUT THE CATS 
+// const filter = (stuff, type) => { ?
+
+const filter = () => {
+// empty array to hold our cats ?********
+  let catArray = [];
+
+// for of loop to loop over that cat array..
+  for (pet of pets) {
+    // if pet type is a cat
+    if (pet.type === "cat") {
+      // if it is then push it into the catarray
+      catArray.push(pet);
+    }
+  }
+
+// now let's use the function we just made above to render our new cat array to the page 
+  renderToDom(catArray);
+};
+
+//event listener stuff. will LISTEN to us to CLICK the button. Upon CLICK it will invoke filter function! 
+catButton.addEventListener("click", filter); 
+
+
+//
+
+
